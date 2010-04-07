4261d0c5f0cb60ce7eed8d5edca7e94c82db5111
lars <lars@localhost.(none)>
2010-04-07 09:01:28 -0700

    Added file which computes a goodness of fit calculation for the Gamma distribution.

---
+++ Projects/Auwahi/gamma_goodness_of_fit.m
@@ -0,0 +1,18 @@
+a = 2.9868;
+b = 75.8201;
+
+% data = gamrnd(a,b,1,10000);
+% data = [0:1:1000];
+load flight_heights
+data = flight_heights;
+
+cdf_sample = 0:1:round(max(data))+1;
+cdf = [cdf_sample' gamcdf(cdf_sample,a,b)'];
+
+figure
+cdfplot(data);
+figure
+hold on
+plot(cdf_sample', gamcdf(cdf_sample,a,b)')
+
+[h,p,ksstat,cv] = kstest(data, cdf, 0.05)
